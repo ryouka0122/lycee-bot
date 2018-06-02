23ed7dd7e3644b9eb1cadd948756c7c5befc24b8
ryouka <coolblossom.0130@gmail.com>
2018-06-02 22:24:04 +0900

    Hubotの実行時に必要なSlackのトークンを外だし

--- reimu/bin/hubot.cmd
+++ reimu/bin/hubot.cmd
@@ -4,6 +4,12 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-SET HUBOT_SLACK_TOKEN=SLACK_TOKEN_KEY
+SET PORT=8081
+
+for /f "delims=" %%t in (./bin/HUBOT_SLACK_TOKEN.txt) do (
+  SET HUBOT_SLACK_TOKEN=%%t
+  goto :break_for
+)
+:break_for
 
 node_modules\.bin\hubot.cmd --name "reimu" %*
