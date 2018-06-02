b78e6e8b356a9d79c4f2c2c2d93db3e670599253
ryouka <coolblossom.0130@gmail.com>
2018-06-02 10:44:21 +0900

    パッケージの安定化

--- fran/bin/hubot.cmd
+++ fran/bin/hubot.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-call npm install
+call npm install --no-audit
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "fran" %* 
+node_modules\.bin\hubot.cmd --name "fran" %*
