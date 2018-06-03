2581c43c1a64be7eb24357c59daece49318d1002
ryouka <coolblossom.0130@gmail.com>
2018-06-03 16:49:04 +0900

    パッケージと起動コマンドをキレイキレイする

--- patchouli/bin/hubot.cmd
+++ patchouli/bin/hubot.cmd
@@ -4,4 +4,12 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "patchouli" %* 
+SET PORT=8082
+
+for /f "delims=" %%t in (./bin/HUBOT_SLACK_TOKEN.txt) do (
+  SET HUBOT_SLACK_TOKEN=%%t
+  goto :break_for
+)
+:break_for
+
+node_modules\.bin\hubot.cmd --name "patchouli" %*
