14a5967c4cac29514f082a043294ece713cfcc46
ryouka <coolblossom.0130@gmail.com>
2018-06-08 07:27:28 +0900

    レミリア（pythonのslackbot）を追加

---
+++ remilia/slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-291993555617-376453222198-ILjYjazutVG3Qj9DF0EoEdgN"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
